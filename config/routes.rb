c780b556b5b0488ccfa4ed2f4b6e74eb999c7dc6
DESKTOP-7I6USA1\MITBo <githubbouya@gmail.com@yahoo.co.jp>
2024-09-18 01:57:23 +0900

    articles controller に変更

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   #get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  
-  root to: 'home#index'
-  get '/about' => 'home#about'
+  root to: 'articles#index'
 
 end
