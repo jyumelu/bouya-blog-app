627a54f772fab406594ebedd15214d7443cbf59a
DESKTOP-7I6USA1\MITBo <githubbouya@gmail.com@yahoo.co.jp>
2024-09-19 14:49:06 +0900

    HEAD show-articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
     def index
         # render 'articles/index'
-        @article = Article.first
+        @articles = Article.all
     end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+    
 end
    
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: 'articles#index'
 
+  resources :articles, only: [:show]
+
 end
