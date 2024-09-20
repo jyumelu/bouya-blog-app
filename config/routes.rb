9025aed63319c4460916a78fc7d4a48fb8be0237
DESKTOP-7I6USA1\MITBo <githubbouya@gmail.com@yahoo.co.jp>
2024-09-20 23:41:23 +0900

    記事を更新できるように変更

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: 'articles#index'
 
-  resources :articles, only: [:show, :new, :create]
+  resources :articles, only: [:show, :new, :create, :edit, :update]
 
 end
