bf818b75e3254f413fc7937c633f07cc3433177c
DESKTOP-7I6USA1\MITBo <githubbouya@gmail.com@yahoo.co.jp>
2024-10-29 04:18:39 +0900

    rich_text_area が表示されない 質問用コミット

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import { Application } from '@hotwired/stimulus';
+import { registerControllers } from '@hotwired/stimulus-loading';
 import "@hotwired/turbo-rails"
-import "controllers"
+
+const application = Application.start();
+registerControllers(application);
